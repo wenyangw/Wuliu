f2b817779e0655f755bbd734e23643a41db0a2cd
wenyangw <wenyangw@gmail.com>
2018-03-10 06:26:21 +0800

    优化Gson

--- app/src/main/java/com/lnyswz/wuliu/common/Utils.kt
+++ app/src/main/java/com/lnyswz/wuliu/common/Utils.kt
@@ -2,18 +2,21 @@ package com.lnyswz.wuliu.common
 
 import android.content.Context
 import android.widget.Toast
+import com.google.gson.Gson
 
 /**
  * Created by Wenyang on 2018/2/18.
  */
 object Utils {
     //val APP_URL = "http://192.168.0.8/lnyswz"
-    //val APP_URL = "http://218.25.74.6/lnyswz"
-    val APP_URL = "http://192.168.0.2:8080"
+    val APP_URL = "http://218.25.74.6/lnyswz"
+    //val APP_URL = "http://192.168.0.2:8080"
 
     //进销存类别的id
     val CATALOG_ID = "aed966ee-e780-4a46-835f-3c6688ec3fd1"
     fun toast(context: Context, message: CharSequence) = Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
+
 }
 
 data class JsonBean(val success: Boolean, val msg: String, val obj: UtilBean)
--- app/src/main/java/com/lnyswz/wuliu/view/LoginActivity.kt
+++ app/src/main/java/com/lnyswz/wuliu/view/LoginActivity.kt
@@ -62,7 +62,8 @@ class LoginActivity : AppCompatActivity() {
         override fun onPostExecute(s: String) {
             super.onPostExecute(s)
 
-            val json = Gson().fromJson(s, JsonBean::class.java)
+            //val json = Gson().fromJson(s, JsonBean::class.java)
+            val json: JsonBean = Gson().fromJson(s)
             if(json.success){
                 enterMain(json)
             }
@@ -70,7 +71,11 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-
+    // 在 kotlin中扩展 Java 类的 Gson.fromJson 方法
+    // 不在传入 T 的class，inline 的作用就是将函数插入到被调用处，配合 reified 就可以获取到 T 真正的类型
+    inline fun <reified T : Any> Gson.fromJson(json: String): T{
+        return fromJson(json, T::class.java)
+    }
 
 }
 
